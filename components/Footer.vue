946844bb0765e4d28457553462203d5bb3c32f3e
Leon Kappes <meluten@gmail.com>
2019-02-22 20:30:15 +0100

    Added Footer

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p class="has-text-white">
+        <strong class="has-text-white">© Copyright(2019)</strong> <a href="/">Leon Kappes</a> available on <a href="https://github.com/LeeDJD/Website-Frontend">Github</a>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
