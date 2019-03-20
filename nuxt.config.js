75dc2ee4871d4907e37c2b19c2714049854fe077
Leon Kappes <meluten@gmail.com>
2019-03-20 21:51:38 +0100

    Fixed output directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Leon Kappes | Backend Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -19,6 +19,13 @@ module.exports = {
   ** Customize the progress-bar color
   */
   loading: { color: '#272727' },
+
+  /*
+  ** Customize the generated output folder
+  */
+  generate: {
+    dir: 'public'
+  },
 
   /*
   ** Global CSS
