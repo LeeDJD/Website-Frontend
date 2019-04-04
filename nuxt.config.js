77861f7bea16e4192594548b70cf0e2a6dd4bfc5
Lee <meluten@gmail.com>
2019-04-04 19:50:05 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
-      { name: 'robots', content: 'noindex'}
+      { name: 'robots', content: 'noindex' }
     ]
   },
 
